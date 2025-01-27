d60cef9c74d1af000aee2a0ee4e9236f069781ee
luiz.arruda <luiz.arruda113@gmail.com>
2025-01-27 14:48:37 -0300

    formatting main

--- Studies/Program.cs
+++ Studies/Program.cs
@@ -10,61 +10,63 @@ public class Program
     public static void Main(string[] args)
     {
         // Big O Example
-        //Console.WriteLine("Big O:");
-        //Console.WriteLine(BigOExamples.SumCharCodes("something"));
-        //Console.WriteLine("---------------------------------------");
+        Console.Write("Big O: ");
+        Console.WriteLine(BigOExamples.SumCharCodes("something"));
+        Console.WriteLine("------------------------------------------------------");
 
         // Array Example
-        //Console.WriteLine("Array:");
-        //int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Array:");
+        int[] numbers = { 1, 2, 3, 4, 5 };
 
         // Accessing elements in the array
-        //Console.WriteLine("First element: " + numbers[0]);
-        //Console.WriteLine("Last element: " + numbers[numbers.Length - 1]);
+        Console.WriteLine("First element: " + numbers[0]);
+        Console.WriteLine("Last element: " + numbers[numbers.Length - 1]);
 
         // Iterating over the array
-        //Console.WriteLine("All elements in the array:");
+        Console.Write("All elements in the array: ");
         //foreach (int number in numbers)
         //{
         //    Console.WriteLine(number);
         //}
-        //Console.WriteLine("---------------------------------------");
+        Console.WriteLine(string.Join(", ", numbers));
+        Console.WriteLine("------------------------------------------------------");
 
         // Linear Search
-        //Console.WriteLine("Linear Search:");
-        //int[] haystack = { 1, 8, 15, 18, 32 };
-        //int needle = 16;
+        Console.Write("Linear Search: ");
+        int[] haystack = { 1, 8, 15, 18, 32 };
+        int needle = 16;
 
-        //Console.WriteLine(LinearSearchExample.Linear_Search(haystack, needle));
-        //Console.WriteLine("---------------------------------------");
+        Console.WriteLine(LinearSearchExample.Linear_Search(haystack, needle));
+        Console.WriteLine("------------------------------------------------------");
 
         // Binary Search
-        //Console.WriteLine("Binary Search:");
-        //int[] binary = { 1, 2, 3, 4, 5, 6, 7 };
-        ////int[] binary= Enumerable.Range(1, 10000).ToArray();
+        Console.Write("Binary Search: ");
+        int[] binary = { 1, 2, 3, 4, 5, 6, 7 };
+        //int[] binary= Enumerable.Range(1, 10000).ToArray();
 
-        //int valueToFind = 4;
+        int valueToFind = 4;
 
-        //Console.WriteLine(BinarySearchExample.Binary_Search(binary, valueToFind));
-        //Console.WriteLine("---------------------------------------");
+        Console.WriteLine(BinarySearchExample.Binary_Search(binary, valueToFind));
+        Console.WriteLine("------------------------------------------------------");
 
         // Two Crystal Balls Problem
-        //Console.WriteLine("Two Crystal Balls Problem:");
+        Console.WriteLine("Two Crystal Balls Problem:");
 
-        //bool[] breaks = new bool[100];
+        bool[] breaks = new bool[100];
 
-        //for (int i = 75; i < breaks.Length; i++)
-        //{
-        //    breaks[i] = true;
-        //}
+        for (int i = 75; i < breaks.Length; i++)
+        {
+            breaks[i] = true;
+        }
 
-        //int result = TwoCrystalBalls.TwoCrystalBallsSolution(breaks);
-        //Console.WriteLine($"Breaking Point Found at Index: {result}");
+        int result = TwoCrystalBalls.TwoCrystalBallsSolution(breaks);
+        Console.WriteLine($"Breaking Point Found at Index: {result}");
+        Console.WriteLine("------------------------------------------------------");
 
         // Bubble Sort
         Console.WriteLine("Bubble Sort:");
 
-        int[] arr = { 1, 4, 5, 9, 7, 2, 3, 6 };
+        int[] arr = { 1, 4, 0, 5, 9, 7, 2, 3, 6, 8 };
 
         Console.Write("Array before sorting: ");
         Console.WriteLine(string.Join(", ", arr));
@@ -73,5 +75,7 @@ public class Program
 
         Console.Write("Array after sorting: ");
         Console.WriteLine(string.Join(", ", arr));
+        Console.WriteLine("------------------------------------------------------");
+
     }
 }
